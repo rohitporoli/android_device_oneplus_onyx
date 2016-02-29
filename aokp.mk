85544c41cc7259f21318a873392f3f9de23f5d87
rohitporoli <rohitp4all@gmail.com>
2016-02-29 12:22:57 +0100

    AOKPify This commit makes the CM device tree, compatible for the Android Open Kang Project
    Change-Id: I969666695c762c6be918467b6698defe82fd7ee9

--- aokp.mk
+++ aokp.mk
@@ -18,13 +18,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
-PRODUCT_NAME := cm_onyx
+PRODUCT_NAME := aokp_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 
