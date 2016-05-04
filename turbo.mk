e5a0ad8921a6f70d2f106d086805432d6de0c3d5
rohitporoli <rohitp4all@gmail.com>
2016-05-04 12:13:54 +0530

    onyx: Turbo Bringup

--- turbo.mk
+++ turbo.mk
@@ -18,13 +18,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
-# Inherit some common Tipsy stuff.
-$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
-PRODUCT_NAME := tipsy_onyx
+PRODUCT_NAME := turbo_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 
