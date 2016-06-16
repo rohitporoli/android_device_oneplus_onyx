04d2b4dc13f0b9eb5b7ffc22b1dad94dc51134e1
Martinusbe <martinusbe@gmail.com>
2016-06-16 12:39:30 +0200

    Revert "Revert "permissive for now""
    This reverts commit 0b7bd4f0f20ee98184eee3fd8aefe6681de06acb.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,7 @@ TARGET_GCC_VERSION_EXP := 4.9
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
