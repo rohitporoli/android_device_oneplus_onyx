f8edbfabb0c4f0bfca4df7a3ce7fd06a3fcb4238
Martinusbe <martinusbe@gmail.com>
2016-08-21 17:38:36 +0200

    tipsy
    Change-Id: Id27b6be85ab003a747dff82aa76b84f807b41930
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- tipsy.mk
+++ tipsy.mk
@@ -19,12 +19,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
-PRODUCT_NAME := slim_onyx
+PRODUCT_NAME := tipsy_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 
