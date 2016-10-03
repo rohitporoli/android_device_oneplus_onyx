07941d12c6f09cbbdc59074ba8e7de18b6e3ab01
SarthakNarang <sarthaknarang109@gmail.com>
2016-10-03 16:23:05 +0530

    onyx: disable sepolicy for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,10 +166,10 @@ BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
 TARGET_NO_RPC := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    $(PLATFORM_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    $(PLATFORM_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
