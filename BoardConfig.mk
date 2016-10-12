f18a1b931e2c8d17e00934ac5673af9f5bee0210
rohitporoli <rohitp4all@gmail.com>
2016-10-13 00:18:55 +0530

    onyx: kill sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,10 +164,10 @@ BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
 TARGET_NEEDS_TEXT_RELOCATIONS := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+# include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    $(PLATFORM_PATH)/sepolicy
+# BOARD_SEPOLICY_DIRS += \
+#     $(PLATFORM_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
