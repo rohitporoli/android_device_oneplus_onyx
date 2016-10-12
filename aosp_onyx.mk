c9ceb542add743b147f0e8c2a05c6dddaf8d4ada
Hamster Tian <updateing@users.noreply.github.com>
2016-10-12 23:40:00 +0530

    onyx: full_base_telephony -> aosp_base_telephony + APN list

--- aosp_onyx.mk
+++ aosp_onyx.mk
@@ -13,7 +13,10 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
