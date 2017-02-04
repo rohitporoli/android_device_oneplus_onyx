6f793697c8ecc58a8e03d49c917bee0498bb1feb
CertifiedBlyndGuy <rjmurdok@linux.com>
2017-02-04 14:42:25 +0530

    onyx: CandyCane's in January

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_onyx-user
-add_lunch_combo lineage_onyx-userdebug
-add_lunch_combo lineage_onyx-eng
+add_lunch_combo candy_onyx-user
+add_lunch_combo candy_onyx-userdebug
+add_lunch_combo candy_onyx-eng
